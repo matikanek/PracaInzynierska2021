bb6e56856bb400b63de79f7e27adab7af0eb37c3
291564.umk@gmail.com <291564.umk@gmail.com>
2021-05-18 00:14:51 +0200

    Rozdział 4.2 - konfiguracja nodemon

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
+  res.send('Welcome to my Nodemon API!');
 });
 
 app.listen(port, () => {
