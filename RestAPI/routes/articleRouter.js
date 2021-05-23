77aa6e2b65c8f381d072ac12ca3d8fe27900dd89
291564.umk@gmail.com <291564.umk@gmail.com>
2021-05-24 01:03:57 +0200

    Rozdział 4.6 - zaimplementowanie metody PATCH i DELETE

--- RestAPI/routes/articleRouter.js
+++ RestAPI/routes/articleRouter.js
@@ -19,31 +19,64 @@ function routes(Article) {
         return res.json(articles);
       });
     });
+  articleRouter.use('/articles/:articleId', (req, res, next) => {
+    Article.findById(req.params.articleId, (err, article) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (article) {
+        req.article = article;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
   articleRouter
     .route('/articles/:articleId')
     .get((req, res) => {
-      Article.findById(req.params.articleId, (err, article) => {
+      res.json(req.article);
+    })
+    .put((req, res) => {
+      const { article } = req;
+      article.CORA_ID = req.body.CORA_ID;
+      article.TITLE = req.body.TITLE;
+      article.YEAR = req.body.YEAR;
+      article.AUTHORS = req.body.AUTHORS;
+      article.CLASS = req.body.CLASS;
+      article.CITED_BY = req.body.CITED_BY;
+      article.CITING = req.body.CITING;
+      article.KEYWORDS = req.body.KEYWORDS;
+      req.article.save((err) => {
         if (err) {
           return res.send(err);
         }
         return res.json(article);
       });
     })
-    .put((req, res) => {
-      Article.findById(req.params.articleId, (err, article) => {
+    .patch((req, res) => {
+      const { article } = req;
+
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        article[key] = value;
+      });
+      req.article.save((err) => {
         if (err) {
           return res.send(err);
         }
-        article.CORA_ID = req.body.CORA_ID;
-        article.TITLE = req.body.TITLE;
-        article.YEAR = req.body.YEAR;
-        article.AUTHORS = req.body.AUTHORS;
-        article.CLASS = req.body.CLASS;
-        article.CITED_BY = req.body.CITED_BY;
-        article.CITING = req.body.CITING;
-        article.KEYWORDS = req.body.KEYWORDS;
-        article.save();
         return res.json(article);
+      });
+    })
+    .delete((req, res) => {
+      req.article.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
       });
     });
 
