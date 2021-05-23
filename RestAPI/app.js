24ab9adf21919eb0b96bfe39b488aa4f467fe0a5
291564.umk@gmail.com <291564.umk@gmail.com>
2021-05-23 21:18:27 +0200

    Rozdział 4.4 - zaimplementowanie metody POST

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/CORA_DS');
@@ -7,15 +8,26 @@ const articleRouter = express.Router();
 const port = process.env.PORT;
 const Article = require('./models/articleModel');
 
-articleRouter.route('/articles').get((req, res) => {
-  const { query } = req;
-  Article.find(query, (err, articles) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(articles);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+articleRouter
+  .route('/articles')
+  .post((req, res) => {
+    const article = new Article(req.body);
+
+    article.save();
+    return res.json(article);
+  })
+  .get((req, res) => {
+    const { query } = req;
+    Article.find(query, (err, articles) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(articles);
+    });
   });
-});
 articleRouter.route('/articles/:articleId').get((req, res) => {
   Article.findById(req.params.articleId, (err, article) => {
     if (err) {
