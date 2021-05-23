b8690e029cdff1f90172224f8807ef272277569e
291564.umk@gmail.com <291564.umk@gmail.com>
2021-05-23 23:19:45 +0200

    Rozdział 4.5 - zaimplementowanie metody PUT

--- RestAPI/routes/articleRouter.js
+++ RestAPI/routes/articleRouter.js
@@ -19,14 +19,33 @@ function routes(Article) {
         return res.json(articles);
       });
     });
-  articleRouter.route('/articles/:articleId').get((req, res) => {
-    Article.findById(req.params.articleId, (err, article) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(article);
+  articleRouter
+    .route('/articles/:articleId')
+    .get((req, res) => {
+      Article.findById(req.params.articleId, (err, article) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(article);
+      });
+    })
+    .put((req, res) => {
+      Article.findById(req.params.articleId, (err, article) => {
+        if (err) {
+          return res.send(err);
+        }
+        article.CORA_ID = req.body.CORA_ID;
+        article.TITLE = req.body.TITLE;
+        article.YEAR = req.body.YEAR;
+        article.AUTHORS = req.body.AUTHORS;
+        article.CLASS = req.body.CLASS;
+        article.CITED_BY = req.body.CITED_BY;
+        article.CITING = req.body.CITING;
+        article.KEYWORDS = req.body.KEYWORDS;
+        article.save();
+        return res.json(article);
+      });
     });
-  });
 
   return articleRouter;
 }
